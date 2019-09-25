43ed33dace9aa308be31ef16896b66af5611dc2a
synapse0426 <synapselee@gmail.com>
2019-09-25 18:12:09 +0900

    9. 스토어 생성

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+// 드디어 리액트 애플리케이션에,  리덕스를 적용하는 단계.!!! 
+
+// 스토어 생성하기.
+const store = createStore(rootReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
