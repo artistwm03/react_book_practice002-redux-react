7b0397ec218a1f2699c08d3b8c2c2f95f7037df1
synapse0426 <synapselee@gmail.com>
2019-09-26 11:40:10 +0900

    15. connect 함수의 두번째 파라미터에 객체 사용해보기.

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { indActionCreators, bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
@@ -7,20 +8,13 @@ const CounterContainer = ({ number, increase, decrease }) => {
   return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
 };
 
-const mapStateToProps = state => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = dispatch => ({
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
-// mapStateToProps : 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수.
-// mapDispatchToProps :액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수.
+
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+  state => ({
+    number: state.counter.number,
+  }),
+  {
+        increase,
+        decrease,
+  },
 )(CounterContainer);
