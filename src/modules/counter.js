87aba3186b556e897acab623c8b02db3c4fab12d
synapse0426 <synapselee@gmail.com>
2019-09-26 16:06:33 +0900

    18. redux-actions 사용-2 리듀서 함수 간단하게 만들기.

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 //yarn add redux-actions 해주고나서 쓰기.
 
 // 리 덕 스 관 련 코 드 작 성
@@ -21,24 +21,33 @@ export const decrease = createAction(DECREASE);
 // 3) 초기화
 //    number 값을 0 으로 초기화,
 const initialState = {
-  number: 0
+  number: 0,
 };
 
 // 4) 리듀서 함수.!! ( counter 라는 리듀서 함수.)
 //    현재 상태를 참고해서 , 새로운 객체를 생성해서 반환해주는 코드작성해줬음.
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1
-      };
-    default:
-      return state;
-  }
-}
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
 // 함수 내보내주기.
 export default counter;
